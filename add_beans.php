ede8591473d08d259e064b2a8bbb40ae113c9ea5
VolatileMint <e203take@gmial.com>
2022-04-22 16:41:27 +0900

    first test

---
+++ add_beans.php
@@ -0,0 +1,38 @@
+<?php
+
+$standard = ["香り"=>"aroma", "酸味"=>"acidry", "甘味"=>"sweetness", "コク"=>"body", "苦味"=>"bitter"];
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<h1>コーヒー豆を追加する</h1>
+<h2>コーヒー豆の情報を入力</h2>
+<form method="POST" action="add_bean_chech.php">
+    銘柄：
+    <input type="text" name="beans_name" required><br>
+    生産地：
+    <input type="text" name="region"><br>
+    <?php foreach($standard as $k=>$v): ?>
+        <?= $k ?>：
+        <select name="<?= $v ?>">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3" selected>3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select><br>
+    <?php endforeach; ?>
+    焙煎温度：
+    <input type="text" name="roasting">℃<br>
+    値段(200gあたり)：
+    <input type="number" name="price" required>円<br>
+    備考<br>
+    <textarea name="memo" cols="30" rows="10"></textarea><br>
+    <input type="submit" value="追加確認">
+</form>
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+
+
+$type = 'mysql';
+$user = 'root';
+$pass = '';
+$host = 'localhost';
+$dbname = 'study';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:dbname={$dbname};host={$host};charset={$charset}";
+$options = [
+    \PDO::ATTR_EMULATE_PREPARES => false, // エミュレート無効
+    \PDO::MYSQL_ATTR_MULTI_STATEMENTS => false, // 複文無効
+    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, // エラー時に例外を投げる(好み)
+];
+try {
+    $dbh = new \PDO($dsn, $user, $pass, $options);
+
+    $table_name = 'beans';
+    $sql = "SELECT * FROM {$table_name} WHERE 1";
+    $pre = $dbh->prepare($sql);
+    $pre->execute();
+    $arr = $pre->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($arr);
+
+}catch( \PDOException $e){
+    echo $e->getMessage(); // XXX 実際は出力しない(logに書くとか)
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>トップ</title>
+</head>
+<body>
+<h1>トップページ</h1>
+<h2>コーヒー評価</h2>
+<a href="add_beans.php">コーヒー豆を追加する</a>
+<a href="review.php">評価する</a>
+
+<?php foreach($arr as $v): ?>
+    <ul>
+        <li>名前：<?= $v['beans_name'];?></li>
+        <li>産地：<?= $v['region'];?></li>
+        <li>香り：<?= $v['aroma'];?></li>
+        <li>酸味：<?= $v['acidty'];?></li>
+        <li>甘さ：<?= $v['sweetness'];?></li>
+        <li>コク：<?= $v['body'];?></li>
+        <li>苦味：<?= $v['bitter'];?></li>
+        <li>焙煎：<?= $v['roasting'];?></li>
+        <p>紹介文：<?= $v['memo'];?></p><br>
+    </ul>
+<?php endforeach; ?>
+
+</body>
+</html>
